48ed7cee03f5ddddce0208bffb8fd95eea909c30
eshaan <eshaanbagga@gmail.com>
2023-08-10 21:02:40 +0530

    Deploying Membook app to GitHub Pages

---
+++ precache-manifest.118c5fb3ca392629893e2bb8d559cd96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64af1468ca7d415dc262b08f2dc839d",
+    "url": "/membook/index.html"
+  },
+  {
+    "revision": "4cae357ca0cbf321628c",
+    "url": "/membook/static/css/main.59851a23.chunk.css"
+  },
+  {
+    "revision": "3d1135fd5706085c6a56",
+    "url": "/membook/static/js/2.1de89497.chunk.js"
+  },
+  {
+    "revision": "dcbbfb5637f2f83a2d41fa93986d1c42",
+    "url": "/membook/static/js/2.1de89497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cae357ca0cbf321628c",
+    "url": "/membook/static/js/main.f65dc8a3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/membook/static/js/main.f65dc8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da1cbad722fc6b86698",
+    "url": "/membook/static/js/runtime-main.13201716.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/membook/static/media/logo.5c0c63fb.png"
+  }
+]);
