dc6855502dc4964236c1b081607899403f6c21a5
eshaan <eshaanbagga@gmail.com>
2023-08-21 13:04:18 +0530

    shifted firebase api to env

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import firebase from "firebase/compat/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBMorcWL_O8yOEnqpj-nHdrLf7w1VKXuoM",
+  apiKey: process.env.FIREBASE_API,
   authDomain: "membook.firebaseapp.com",
   projectId: "membook",
   storageBucket: "membook.appspot.com",
